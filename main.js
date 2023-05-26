87bca1422f00ead1da5a64ba3954611c285218dc
Kittenexplode <124947023+Kittenexplode@users.noreply.github.com>
2023-05-25 17:52:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,91 +1,108 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
-//Define the width & height of the rover image.
-rover_width = 100
-rover_height = 90
-background_image = "mars.jpg";
+nasa_mars_images_array = ["nasa_image_1.jpg","nasa_image_2.jpeg", "nasa_image_3.jpg","nasa_image_4.jpg"];
 
+random_number = Math.floor(Math.random() * 4);
+console.log(random_number);
+rover_width = 100;
+rover_height = 90;
+
+background_image = nasa_mars_images_array[random_number];
+console.log("background_image = " + background_image);
 rover_image = "rover.png";
 
 rover_x = 10;
 rover_y = 10;
 
-//Create "add()" function
-     function add() {
-        background_imgTag = new Image();
-        background_imgTag.onload = uploadBackground;
-        background_imgTag.src=background_image;
-        
-       rover_imgTag = new Image();
-       rover_imgTag.onload = uploadrover;
-        rover_imgTag.src=rover_image; 
-     }                        
-//Create "uploadBackground()" function.
-        function uploadBackground()
-        {
-            ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
-        }                      
+function add() {
+	background_imgTag = new Image(); //defining a variable with a new image
+	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+	background_imgTag.src = background_image;   // load image
 
-//Create "uploadrover()" function.
-function uploadrover()
-{
-ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+	rover_imgTag = new Image(); //defining a variable with a new image
+	rover_imgTag.onload = uploadrover; // setting a function, onloading this variable
+	rover_imgTag.src = rover_image;   // load image
 }
 
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover() {
+	ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+
+
 window.addEventListener("keydown", my_keydown);
+
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-    //Write code if keypressed is up. ASCII value of "up" arrow is 38.
-		
-        if(keyPressed =='38')
-      {
-    up();
-        console.log("up");
-      }
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+}
 
+function up()
+{
+	if(rover_y >=0)
+	{
+		rover_y = rover_y - 10;
+		console.log("When up arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		 uploadBackground();
+		 uploadrover();
+	}
+}
+function down()
+{
+	if(rover_y <=500)
+	{
+		rover_y =rover_y+ 10;
+		console.log("When down arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
+}
 
-     //Write code if keypressed is down. ASCII value of "up" arrow is 40.
-        if(keyPressed =='37')
-     {
-        left();
-       console.log("left");
-     }
-
-    if(keyPressed =='40')
-   {
-    down();
-     console.log("down");
+function right()
+{
+	if(rover_x <= 700)
+	{
+		rover_x =rover_x + 10;
+		console.log("When right arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();
    }
- 
-    if(keyPressed =='49')
-   {
-    right();
-   console.log("right");
-   }
-
-
-
-
-
-    //Additional Activity
-    //Write the code for left and right arrow pressed. 
 }
-
-function up(){
-
+//Additional Activity
+function left()
+{
+	if(rover_x >= 0)
+	{
+		rover_x =rover_x - 10;
+		console.log("When left arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
 }
-function down(){
+//Additional Activity ends here.
 	
-}
-function right(){
-	
-}
-function left(){
-	
-}
-
-
-
